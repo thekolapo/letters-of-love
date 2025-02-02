f6bf70cf308f2a47024f6c37a8b419417ec0412f
Kolapo Oni <onikolapo@gmail.com>
2025-02-02 04:53:55 +0100

    implement static home and read-only letter page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,27 @@
 <template>
-	<div>
-		jjjj
+	<div class="home">
+		<p class="u-font-normal">
+			Send beautiful letters to loved ones&nbsp;
+			<span class="u-font-style-normal">🌺️</span>
+		</p>
+		<div class="home__letters">
+			<p class="home__letters-title">Featured Letters</p>
+			<div class="home__letters-list">
+				<div class="home__letter">
+					<nuxt-link to="letter/mama" class="u-font-normal">A love letter from Jane Doe</nuxt-link>
+					<div class="home__letter-date">12 hours ago</div>
+				</div>
+				<div class="home__letter">
+					<nuxt-link to="letter/mama" class="u-font-normal">I love you mama</nuxt-link>
+					<div class="home__letter-date">2 days ago</div>
+				</div>
+				<div class="home__letter">
+					<nuxt-link to="letter/mama" class="u-font-normal">I wish you the very best in life , always</nuxt-link>
+					<div class="home__letter-date">2 days ago</div>
+				</div>
+			</div>
+		</div>
+		<button class="c-button c-button--add">+</button>
 	</div>
 </template>
   
@@ -8,5 +29,38 @@
 </script>
   
 <style lang="scss" scoped>
+.home {	
+	&__letters {
+		margin-top: 80px;
 
+		&-title {
+			text-transform: uppercase;
+			// text-decoration: underline;
+			color: var(--color-gray);
+		}
+
+		&-list {
+			display: grid;
+			gap: 44px;
+			margin-top: 26px;
+		}
+	}
+
+	&__letter {
+		a {
+			transition: 0.3s color linear;
+			text-decoration: none;
+			color: inherit;
+
+			&:hover {
+				color: white;
+			}
+		}
+
+		&-date {
+			margin-top: 12px;
+			color: var(--color-gray);
+		}
+	}
+}
 </style>
---
+++ pages/letter/[slug].vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="letter">
+		<p class="u-font-normal">
+			A love letter from John Doe&nbsp;
+			<span class="u-font-style-normal">🌺️</span>
+		</p>
+		<textarea ref="textArea" class="letter__body u-font-md" readonly>Jane Doe,
+
+I’ll love you to the end of time and till my dying breath. When I’m with you, life is perfect and time ceases to exist. 
+
+Love from,
+John Doe
+		</textarea>
+	</div>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue';
+
+const textArea = ref(null);
+
+const autoResize = (textarea) => {
+	textarea.style.height = `${textarea.scrollHeight}px`;
+}
+
+onMounted(() => {
+	if(textArea.value) autoResize(textArea.value);
+});
+</script>
+  
+<style lang="scss" scoped>
+.letter {
+	display: flex;
+	flex-direction: column;
+
+	&__body {
+		margin-top: 67px;
+		width: 100%;
+		max-width: 900px;
+		background: transparent;
+		border: none;
+		color: inherit;
+		padding: 0;
+		min-height: 350px;
+		white-space: pre-wrap;
+		resize: none;
+	}
+}
+</style>
