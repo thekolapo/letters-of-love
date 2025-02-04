8a9891d7e8e2e4202727f9e89ffdeab6c23612bc
Kolapo Oni <onikolapo@gmail.com>
2025-02-04 06:14:17 +0100

    add meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,4 +14,25 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  app: {
+    head: {
+      title: 'Letters of Love',
+      meta: [
+        { name: 'description', content: 'Send beautiful letters to loved ones' },
+        { property: 'og:title', content: 'Letters of Love' },
+        { property: 'og:description', content: 'Send beautiful letters to loved ones' },
+        { property: 'og:image', content: 'https://lettersoflove.netlify.app/meta-image.png' },
+        { property: 'og:image:width', content: '1200' },
+        { property: 'og:image:height', content: '630' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:title', content: 'Letters of Love' },
+        { name: 'twitter:description', content: 'Send beautiful letters to loved ones' },
+        { name: 'twitter:image', content: 'https://lettersoflove.netlify.app/meta-image.png' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  }
 })
