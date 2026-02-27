062d1133d6bd9bff3c9c7d46e0fbc4ad7bb7f40a
Kolapo Oni <onikolapo@gmail.com>
2026-02-27 05:02:43 +0100

    refactor code to improve naming semantics

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,58 @@
 <template>
-	<div class="home">
-		<p class="u-font-normal">
-			Send beautiful letters to loved ones&nbsp;
-			<span class="u-font-style-normal">🌺️</span>
-		</p>
-		<div class="home__letters">
-			<p class="home__letters-title">Featured Letters</p>
-			<div class="home__letters-list">
-				<div class="home__letter" v-for="(letter, index) in featuredLetters" :key="index">
-					<nuxt-link :to="{ name: 'letter-slug', params: { slug: letter.slug } }" class="u-font-normal">{{ letter.title }}</nuxt-link>
-					<div class="home__letter-date">{{ useTimeAgo(letter.createdAt._seconds) }}</div>
-				</div>
-			</div>
-		</div>
-		<nuxt-link :to="{name: 'new-letter'}" class="c-button c-button--add">+</nuxt-link>
-	</div>
+  <div class="home">
+    <h1 class="u-font-normal">
+      Send beautiful letters to loved ones&nbsp;
+      <span class="u-font-style-normal">💌 🌺️</span>
+    </h1>
+    <section class="home__letters">
+      <h2 class="home__letters-title">Featured Letters</h2>
+      <ul class="home__letters-list">
+        <li
+          class="home__letter"
+          v-for="(letter, index) in featuredLetters"
+          :key="index"
+        >
+          <nuxt-link
+            :to="{ name: 'letter-slug', params: { slug: letter.slug } }"
+            class="u-font-normal"
+            >{{ letter.title }}</nuxt-link
+          >
+          <span class="home__letter-date">
+            {{ useTimeAgo(letter.createdAt._seconds) }}
+          </span>
+        </li>
+      </ul>
+    </section>
+    <nuxt-link :to="{ name: 'new-letter' }" class="c-button c-button--add">
+      +
+    </nuxt-link>
+  </div>
 </template>
-  
+
 <script setup>
-import { useAsyncData } from '#app'
-import { useApi } from '~/composables/api';
+import { useAsyncData } from "#app";
+import { useApi } from "~/composables/api";
 
 const api = useApi();
 
-const { data: featuredLetters, pending, error, refresh } = await useAsyncData('lettersData', async () => {
-  const response = await api.fetchFeaturedLetters(); 
-  const featuredLetters = response.data.data
+const {
+  data: featuredLetters,
+  pending,
+  error,
+  refresh,
+} = await useAsyncData("lettersData", async () => {
+  const response = await api.fetchFeaturedLetters();
+  const featuredLetters = response.data.data;
 
-  return featuredLetters.sort((a, b) => b.createdAt._seconds - a.createdAt._seconds); 
+  return featuredLetters.sort(
+    (a, b) => b.createdAt._seconds - a.createdAt._seconds
+  );
 });
 
 const formatDate = (seconds) => {
   const date = new Date(seconds * 1000);
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  return date.toLocaleDateString("en-US", options);
 };
 
 const useTimeAgo = (timestamp) => {
@@ -67,48 +86,51 @@ const useTimeAgo = (timestamp) => {
   return "just now";
 };
 </script>
-  
+
 <style lang="scss" scoped>
-.home {	
-	&__letters {
-		margin-top: 80px;
+.home {
+  &__letters {
+    margin-top: 80px;
 
     @media screen and (max-width: 480px) {
       margin-top: 50px;
-	  }
+    }
 
-		&-title {
-			text-transform: uppercase;
-			// text-decoration: underline;
-			color: var(--color-gray);
-		}
+    &-title {
+      text-transform: uppercase;
+      // text-decoration: underline;
+      color: var(--color-gray);
+    }
 
-		&-list {
-			display: grid;
-			gap: 44px;
-			margin-top: 26px;
-		}
-	}
+    &-list {
+      display: grid;
+      gap: 44px;
+      margin-top: 26px;
+    }
+  }
 
-	&__letter {
-		a {
-			transition: 0.3s color linear;
-			text-decoration: none;
-			color: inherit;
+  &__letter {
+    display: grid;
+    gap: 12px;
 
-			&:hover {
-				color: white;
-			}
-		}
+    @media screen and (max-width: 480px) {
+      gap: 12px;
+    }
 
-		&-date {
-			margin-top: 12px;
-			color: var(--color-gray);
+    a {
+      transition: 0.3s color linear;
+      text-decoration: none;
+      color: inherit;
+      width: fit-content;
 
-      @media screen and (max-width: 480px) {
-        margin-top: 8px;
+      &:hover {
+        color: white;
       }
-		}
-	}
+    }
+
+    &-date {
+      color: var(--color-gray);
+    }
+  }
 }
 </style>
