9640abd7dd8c71141a98859dc3918f968b8e2da8
Kolapo Oni <onikolapo@gmail.com>
2026-03-04 15:49:43 +0100

    fix letter page rendering issue

--- pages/letter/[slug].vue
+++ pages/letter/[slug].vue
@@ -4,14 +4,19 @@
       {{ letter.title }}&nbsp;
       <!-- <span class="u-font-style-normal">🌺️</span> -->
     </p>
-    <textarea ref="textArea" class="letter__body u-font-md" readonly v-model="letter.body" />
+    <textarea
+      ref="textArea"
+      class="letter__body u-font-md"
+      readonly
+      v-model="letter.body"
+    />
   </div>
 </template>
- 
+
 <script setup>
-import { useAsyncData, useRoute } from '#app';
-import { ref, onMounted } from 'vue';
-import { useApi } from '~/composables/api';
+import { useAsyncData, useRoute } from "#app";
+import { ref, onMounted } from "vue";
+import { useApi } from "~/composables/api";
 import cryptography from "~/utils/cryptography";
 
 const api = useApi();
@@ -22,13 +27,17 @@ const autoResize = (textarea) => {
   textarea.style.height = `${textarea.scrollHeight}px`;
 };
 
-const { data: letter, pending, error } = await useAsyncData('letterData', async () => {
+const {
+  data: letter,
+  pending,
+  error,
+} = await useAsyncData("letterData", async () => {
   const slug = route.params.slug;
   const response = await api.fetchLetter(slug);
 
-  const letterData = response.data.data;
+  const letterData = response.data;
   // const decryptedBody = await cryptography.decrypt(letterData.body);
-  // letterData.body = decryptedBody.replace(/\\n/g, "\n"); 
+  // letterData.body = decryptedBody.replace(/\\n/g, "\n");
   return letterData;
 });
 
